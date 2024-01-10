d1f7d9370eada6dc93c6efbcb7610b154eb0e1bc
adityjha0 <aditya2005aps@gmail.com>
2024-01-10 12:01:12 +0530

    odd

--- odd.ml
+++ odd.ml
@@ -8,3 +8,15 @@ let l = [1;3;5;7;9];;
 
 let () = List.iter print_int (odd_elements l);;
 let () = print_newline();;
+
+(* better approach *)
+
+let rec odd_elements_b l = 
+  match l with
+    h::_::t -> h :: odd_elements_b t
+  | _ -> l;;
+
+
+let () = List.iter print_int (odd_elements_b l);;
+let () = print_newline();;
+  
