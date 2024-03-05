baaace93b53f8b985bc210dd68f4b26f109b6e36
adityjha0 <aditya2005aps@gmail.com>
2024-03-06 03:10:01 +0530

    cleaner

--- hanoi.ml
+++ hanoi.ml
@@ -1,10 +1,6 @@
 let rec hanoi a b c n =
   if n == 1 then
     Printf.printf "%d to %d\n" a c
-  else if n == 2 then
-    Printf.printf "%d to %d\n%d to %d\n%d to %d\n" a b a c b c
-  else if n == 3 then
-    Printf.printf "%d to %d\n%d to %d\n%d to %d\n%d to %d\n%d to %d\n%d to %d\n%d to %d\n" a c a b c b a c b a b c a c
   else begin
     hanoi a c b (n-1);
     Printf.printf "%d to %d\n" a c;
@@ -15,4 +11,5 @@ let rec hanoi a b c n =
 
 let () = 
   let n = read_int() in
-  hanoi 1 2 3 n;;
+  hanoi 1 2 3 n
+;;
