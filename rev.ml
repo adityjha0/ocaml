55d5bed7ee886cbe1b6b4060ff1085aa47d89288
adityjha0 <aditya2005aps@gmail.com>
2024-03-10 12:54:56 +0530

    efficient rev

--- rev.ml
+++ rev.ml
@@ -1,9 +1,21 @@
 let rec rev l = 
   match l with
     []->[]
-  | h::t -> (rev t) @ [h];;
+  | h::t -> (rev t) @ [h]
+;;
+
+(* This is inefficient, lets try an efficient approach *)
+
+let reverse l =
+	let rec helper l acc = 
+    match l with 
+    | [] -> acc
+    | h::t -> helper t (h::acc)
+  in
+  helper l []
+;;
 
 let list = [1;2;3;4;5;6];;
 
-let () = List.iter print_int(rev list);;
+let () = List.iter print_int(reverse list);;
 let () = print_newline();;
